bec481050c67e822ed57f37400dcc6bf3105710f
jabz123 <142000415+jabz123@users.noreply.github.com>
2026-03-16 04:16:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ function score_class(int $score): string {
         </a>
         <div class="nav-links" id="nav-links">
             <a href="#features">Features</a>
-            <a href="#how-it-works">How It Workssss</a>
+            <a href="#how-it-works">How It Worksssss</a>
             <a href="#testimonials">Testimonials</a>
             <a href="#pricing">Pricing</a>
         </div>
