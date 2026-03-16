6c71d2f579c42a6f44e1af40ffb6dfe57c4596cc
Jerel03 <Imjerel2003@gmail.com>
2026-03-16 17:40:29 +0800

    Remove editor role check and only include system_admin check

--- includes/layout.php
+++ includes/layout.php
@@ -1,13 +1,6 @@
 <?php
-// Updated role-based sidebar logic checking for system_admin
-if ($user_role === 'system_admin') {
-    echo '<div class="badge badge-system-admin">System Administrator</div>';
-    // Sidebar items for system_admin
-} elseif ($user_role === 'editor') {
-    echo '<div class="badge badge-editor">Editor</div>';
-    // Sidebar items for editor
-} else {
-    echo '<div class="badge badge-user">User</div>';
-    // Sidebar items for regular users
+// Previous code
+if ($user->hasRole('system_admin')) {
+    // Grants access to system admins
+    grantAccess();
 }
-?>
