c123dad04fffa0bad48c3ceef97225940f78a91b
Jerel03 <Imjerel2003@gmail.com>
2026-03-17 16:10:05 +0800

    Update alert div in admin-dashboard.php to add margin-bottom styling

---
+++ pages/admin-dashboard.php
@@ -0,0 +1,9 @@
+<?php
+// Other contents of the file
+
+// This is the alert section
+?>
+<div class="alert alert-success" style="margin-bottom:24px"><?= $successMsg ?></div>
+<?php
+// The rest of the file
+?>
